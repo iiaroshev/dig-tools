272f8260417e2a51d8319c61b5bd52380f44566a
iiaroshev <116879337+iiaroshev@users.noreply.github.com>
2022-12-20 17:09:45 +0100

    m

--- week2/data_plotting.py
+++ week2/data_plotting.py
@@ -3,15 +3,13 @@ import pandas as pd
 url = 'https://raw.githubusercontent.com/ipozdeev/it-skills-for-research/master/data/coding-environment-exercise.csv'
 df = pd.read_csv(url)
 
-# make up some data
 x = df.date
 y = df.value
 
-# plot
+#making up the plot
 plt.plot(x,y)
 plt.gcf().autofmt_xdate()
 
 plt.show()
 
-#Saving the plot
 plt.savefig('first plot.jpg', bbox_inches='tight', dpi=150)
